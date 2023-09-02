e7497d3307740c0062be2cc20107f5c4fc1b459f
kevinnls <developer@kevinnlsamuel.com>
2023-09-02 13:20:46 +0530

    add dummy about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Privacy Policy</h1>
+This web app works entirely offline except for the inital load
+and any updates to itself. All data is stored only on the device.
+Nothing is shared back to the owner or a third party.
+
