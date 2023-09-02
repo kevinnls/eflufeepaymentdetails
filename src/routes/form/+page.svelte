484c1a0758bf254ec6dfffa540a10e35f03e783a
kevinnls <developer@kevinnlsamuel.com>
2023-09-02 12:16:57 +0530

    create base form

---
+++ src/routes/form/+page.svelte
@@ -0,0 +1,26 @@
+<p>
+	Fill in your payment details
+</p>
+
+<form class="grid grid-cols-[2fr_3fr] w-4/5 md:w-2/3 gap-2 md:gap-4 mx-auto">
+	<label for="programme">Pick your programme</label>
+	<select id="programme">
+		<option value="ug">UG Undergraduate</option>
+		<option value="pg">PG Postgraduate</option>
+		<option value="phd">PhD Doctorate</option>
+	</select>
+
+	<label for="sem">Semester</label>
+	<select id="sem">
+		<option value="1">Sem I</option>
+		<option value="2">Sem II</option>
+		<option value="3">Sem III</option>
+		<option value="4">Sem IV</option>
+		<option value="5">Sem V</option>
+		<option value="6">Sem VI</option>
+		<option value="7">Sem VII</option>
+		<option value="8">Sem VIII</option>
+		<option value="9">Sem IX</option>
+		<option value="10">Sem X</option>
+	</select>
+</form>
