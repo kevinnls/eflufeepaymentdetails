ac4e71106b1538e29c10ab361cfe587661df8273
kevinnls <developer@kevinnlsamuel.com>
2023-10-12 22:35:08 +0530

    install some stuff for 11ty
    markdown attributes
    configure eleventy renderFile plugin

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,8 +1,14 @@
-const {	EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const {	EleventyHtmlBasePlugin, EleventyRenderPlugin } = require("@11ty/eleventy");
+const markdownItAttrs = require("markdown-it-attrs");
 
 module.exports = function(eleventyConfig){
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
+
+	eleventyConfig.amendLibrary("md", mdLib => mdLib.use(markdownItAttrs));
+
 	eleventyConfig.addPassthroughCopy("static");
+
 	return {
 		pathPrefix: "/",
 		dir: {
